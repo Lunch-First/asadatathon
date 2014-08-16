12a5a44b8376fcc344cd0eab37d2443b9537ed20
Roy Keyes <roy.coding@gmail.com>
2014-08-16 10:50:29 -0700

    Added parsed Eric Garner text files.

--- lexisnexis_split.sh
+++ lexisnexis_split.sh
@@ -1,5 +1,6 @@
 mkdir decision
 csplit -f tm-decision- -n 4 Trayvon_Martin_Decision_Combined_Papers2014-08-15_22-08.HTML '/</DOC>/'1 {*}
+# Last file is just the bottom HTML tags, no content
 rm decision/tm-decision-0500
 
 mkdir trial1
@@ -13,3 +14,8 @@ rm trial2/tm-trial2-0500
 mkdir stories
 csplit -f stories/tm-stories- -n 4 Trayvon_Stories1_Combined_Papers2014-08-15_21-51.HTML '/</DOC>/'1 {*}
 rm stories/tm-stories-0400
+
+
+mkdir EricGarner_1_1
+csplit -f EricGarner_1_1/eg_1_1- -n 4 EricGarner_1_1.HTML '/</DOC>/'1 {*}
+rm EricGarner_1_1/eg_1_1-0168
