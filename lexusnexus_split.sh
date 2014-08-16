a0c90eea8f0428bc2df4e395ef5f259173c1d483
Roy Keyes <roy.coding@gmail.com>
2014-08-15 22:05:34 -0700

    Added scripts to split and parse Lexus Nexus HTML and reulting text files

---
+++ lexusnexus_split.sh
@@ -0,0 +1,15 @@
+mkdir decision
+csplit -f tm-decision- -n 4 Trayvon_Martin_Decision_Combined_Papers2014-08-15_22-08.HTML '/</DOC>/'1 {*}
+rm decision/tm-decision-0500
+
+mkdir trial1
+csplit -f trial1/tm-trial1- -n 4 Trayvon_Martin_Trial1_Combined_Papers2014-08-15_21-59.HTML '/</DOC>/'1 {*}
+rm trial1/tm-trial1-0500
+
+mkdir trial2
+csplit -f trial2/tm-trial2- -n 4 Trayvon_Martin_Trial2_Papers2014-08-15_22-01.HTML '/</DOC>/'1 {*}
+rm trial2/tm-trial2-0500
+
+mkdir stories
+csplit -f stories/tm-stories- -n 4 Trayvon_Stories1_Combined_Papers2014-08-15_21-51.HTML '/</DOC>/'1 {*}
+rm stories/tm-stories-0400
